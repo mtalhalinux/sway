61a9e81c8a06f7c08cba38f7f084cf6ab63a74bc
mtalhalinux <117022897+mtalhalinux@users.noreply.github.com>
2022-12-06 20:28:53 +0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+printf 'y'| sudo apt update 
+printf 'y' |sudo apt install xorg
+printf 'y' |sudo apt install pulseaudio
+printf 'y' |sudo apt install alsa-utils
+printf 'y' |sudo apt install alsa-utils
+printf 'y' |sudo apt install pavucontrol
+printf 'y' |sudo apt install compton
+printf 'y' |sudo apt install picom
+printf 'y' |sudo apt install kitty
+printf 'y' |sudo apt install network-manager-gnome
+printf 'y' |sudo apt instal arandr
+printf 'y' |sudo apt install geeqie
+printf 'y' |sudo apt install wget
+printf 'y' |sudo apt install htop
+printf 'y' |sudo apt install hardinfo
+printf 'y' |sudo apt install lxappearance
+printf 'y' |sudo apt install neofetch
+printf 'y' |sudo apt install  volumeicon-alsa
+printf 'y' |sudo apt install galculator
+printf 'y' |sudo apt autoclean 
+printf 'y' |sudo apt autoremove
+printf 'y' |sudo apt update
