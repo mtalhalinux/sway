944c30f6b18c1944d73baff6bc99ea3287803441
mtalhalinux <117022897+mtalhalinux@users.noreply.github.com>
2022-12-13 20:48:16 +0500

    Update installi3.sh

--- installi3.sh
+++ installi3.sh
@@ -51,4 +51,4 @@ cd ~
 echo "Installing free download manager"
 wget https://dn3.freedownloadmanager.org/6/latest/freedownloadmanager.deb
 sudo dpkg -i freedownloadmanager.deb
-
+sudo apt install -f
